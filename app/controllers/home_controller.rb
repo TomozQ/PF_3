57154b819ae6394997527bca386faf481999367a
TomozQ <inf.4clover@gmail.com>
2020-10-08 12:13:20 +0900

    PC側

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,6 @@ class HomeController < ApplicationController
   def staff
   end
 
+  def gallery
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#top' 
   get '/menu', to: 'home#menu'
   get '/staff', to: 'home#staff'
+  get '/gallery', to: 'home#gallery'
 end
